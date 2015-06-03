13ff65f2c3a4e244fb8092046456298e86a15e2b
gitdropcode <dzguangdai@gmail.com>
2015-06-03 14:03:02 -0400

    Create graphical components

--- src/com/gitdropcode/numberguess/Window.java
+++ src/com/gitdropcode/numberguess/Window.java
@@ -1,17 +1,54 @@
 package com.gitdropcode.numberguess;
 
+import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
 import java.net.URL;
 
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class Window extends JFrame
 {
 	private static final long serialVersionUID = 1L;
 
+	/*
+	 * LAYOUT:
+	 * 
+	 *     timer    | buttonPane
+	 *  ------------------------
+	 *     entry    |    log
+	 */
+	private JPanel timer;
+	private JLabel timerLab;
+	
+	private JPanel buttonPane;
+	private JButton startButton;
+	private JButton enterButton;
+	private JButton ansButton;
+	private JButton instButton;
+	
+	private JPanel entry;
+	private JTextField entryField;
+	
+	private JPanel log;
+	private JScrollPane logScroll;
+	private JTextArea logArea;
+	
 	public Window()
 	{
+		addComponents();
+		
 		getContentPane().setPreferredSize(new Dimension(800, 600));
 		pack();
 		setLocationRelativeTo(null);
@@ -24,6 +61,83 @@ public class Window extends JFrame
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
+	}
+	
+	private void addComponents()
+	{
+		timerLab = new JLabel("TIMER");
+		Font timerFont = timerLab.getFont();
+		timerLab.setFont(timerFont.deriveFont(Font.PLAIN, 96.0f));
+		
+		timer = new JPanel();
+		BoxLayout timerLayout = new BoxLayout(timer, BoxLayout.X_AXIS);
+		timer.setLayout(timerLayout);
+		timer.add(Box.createHorizontalGlue());
+		timer.add(timerLab);
+		timer.add(Box.createHorizontalGlue());
+		
+		startButton = new JButton("Start Game");
+		enterButton = new JButton("Enter");
+		ansButton = new JButton("Show Answer");
+		instButton = new JButton("Instructions");
+		
+		buttonPane = new JPanel();
+		BoxLayout buttonLayout = new BoxLayout(buttonPane, BoxLayout.Y_AXIS);
+		buttonPane.setLayout(buttonLayout);
+		buttonPane.add(Box.createVerticalGlue());
+		addAButton(startButton);
+		buttonPane.add(Box.createVerticalGlue());
+		addAButton(enterButton);
+		buttonPane.add(Box.createVerticalGlue());
+		addAButton(ansButton);
+		buttonPane.add(Box.createVerticalGlue());
+		addAButton(instButton);
+		buttonPane.add(Box.createVerticalGlue());
+		
+		entryField = new JTextField();
+		entryField.setHorizontalAlignment(JTextField.CENTER);
+		Font entryFont = entryField.getFont();
+		entryField.setFont(entryFont.deriveFont(Font.PLAIN, 120.0f));
+		entryField.setMinimumSize(new Dimension(350, 200));
+		entryField.setPreferredSize(new Dimension(350, 200));
+		entryField.setMaximumSize(new Dimension(350, 200));
+		
+		entry = new JPanel();
+		BoxLayout entryLayout = new BoxLayout(entry, BoxLayout.X_AXIS);
+		entry.setLayout(entryLayout);
+		entry.add(Box.createHorizontalGlue());
+		entry.add(entryField);
+		entry.add(Box.createHorizontalGlue());
+		
+		logArea = new JTextArea("log");
+		logArea.setEditable(false);
+		logArea.setFocusable(false);
+		Font logFont = logArea.getFont();
+		logArea.setFont(logFont.deriveFont(Font.PLAIN, 24.0f));
+		
+		logScroll = new JScrollPane(logArea);
+		logScroll.setPreferredSize(new Dimension(400, 300));
+		logScroll.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
+		
+		log = new JPanel();
+		log.add(logScroll);
+		
+		setLayout(new GridLayout(2, 2));
+		add(timer);
+		add(buttonPane);
+		add(entry);
+		add(log);
+	}
+	
+	private void addAButton(JButton button)
+	{
+		button.setAlignmentX(Component.CENTER_ALIGNMENT);
+		Font buttonFont = button.getFont();
+		button.setFont(buttonFont.deriveFont(Font.PLAIN, 24.0f));
+		button.setMinimumSize(new Dimension(200, 60));
+		button.setMaximumSize(new Dimension(200, 60));
+		button.setPreferredSize(new Dimension(200, 60));
+		buttonPane.add(button);
 	}
 
 	public static void main(String[] args)
